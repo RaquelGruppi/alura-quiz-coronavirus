b6d4699bf9b0d72bfe93df8670754c41f6c94e32
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-01-27 10:54:08 +1100

    Final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import styled from 'styled-components'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Widget from '../src/components/Widget';
+import db from '../db.json';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    margin-top: 100px;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg} >
+      <QuizContainer>
+      <Widget>
+        <Widget.Header>
+          <h1>Coviv-19</h1>
+        </Widget.Header>
+        <Widget.Content>
+          <p>Teste seus conhecimento sobre o coronavírus</p>
+        </Widget.Content>
+      </Widget>
+      <Widget>
+        <Widget.Content>
+          <h1>Quizes da galera</h1>
+          <p>Texto</p>
+        </Widget.Content>
+      </Widget>
+      <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="http://github.com/r-gruppi"/>
+    </QuizBackground>)
 }
---
+++ src/components/QuizBackground/index.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+const QuizBackground = styled.div`
+  width: 100%;
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.Bg};
+  position: relative;
+  
+  &:after {
+    content: "";
+    background-size: auto 500px;
+    background-repeat: no-repeat;   
+    background-position: center 0;
+    background-image: url(${({ backgroundImage }) => backgroundImage}); */
+    display: block;
+    width: 550px;
+    height: 275px;
+    border-radius: 275px 275px 0 0;
+    position: absolute;
+    bottom: 0px;
+    left: -250px;
+    transform: rotateX('180');
+    opacity: 0.2;
+    z-index: 1;
+  }
+  &:before {
+    content: "";
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-image: url(${({ backgroundImage }) => backgroundImage});
+    display: block;
+    width: 530px;
+    height: 535px;
+    position: absolute;
+    top: 50%;
+    left: 60vw;
+    z-index: 1;
+    transform: translateY(-50%);
+    clip-path: circle(270px);
+  }
+
+  *:first-child {
+      position: relative;
+      z-index: 10;
+    }
+  @media screen and (max-width: 500px) {
+    &:after {
+    background-image: none;
+    display: none;
+    }
+    &:before {
+      background-size: auto 100%;
+      background-position: center;
+      background-image:
+        linear-gradient(transparent, ${({ theme }) => theme.colors.Bg}),
+        url(${({ backgroundImage }) => backgroundImage});
+      width: 230px;
+      height: 210px;
+      top: 0;
+      right: 0;
+      transform: translateY(0%);
+      transform: translatex(-50%);
+      clip-path: circle(110px);
+    }
+    *:first-child {
+      position: relative;
+      z-index: 10;
+    }
+  }
+`;
+
+export default QuizBackground;
