b8f46353cba58316b689808875f7586024837b54
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-01-27 15:31:40 +1100

    Final da segunda aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,16 +25,21 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const { theme } = db;
 
-const theme = db.theme;
-
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
